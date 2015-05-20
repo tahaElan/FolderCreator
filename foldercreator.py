07193b82a53db276935a83111fd82beb5d27a4d1
tahaElan <taha2506@yahoo.com>
2015-05-20 13:37:41 +0530

    Update foldercreator.py

--- foldercreator.py
+++ foldercreator.py
@@ -3,6 +3,6 @@ print "enter the path where the folders should be created "
 s=raw_input()
 for i in range(1,101):
 	os.mkdir(s+"/folder"+str(i))
-	with open(s+"/folder"+str(i)+"/file"+str(i)+".txt", "wt") as myfile:
+	with open(s+"/folder"+str(i)+"/folder"+str(i)+".txt", "wt") as myfile:
 		os.chmod(s+"/folder"+str(i)+"/file"+str(i)+".txt", 0700)
 		
